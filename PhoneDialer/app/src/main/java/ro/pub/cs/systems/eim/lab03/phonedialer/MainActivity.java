de2b47e07f1ca4e7e706436a9f58fb6998a9b9c4
Root USO User <student@stud.acs.upb.ro>
2019-03-08 15:51:06 +0200

    pana la backspace button

--- PhoneDialer/app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/MainActivity.java
+++ PhoneDialer/app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/MainActivity.java
@@ -2,12 +2,66 @@ package ro.pub.cs.systems.eim.lab03.phonedialer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText phoneNumberEditText;
+    private ImageButton backspaceImageButton;
+    private Button genericButton;
+
+    final static int number_buttons[] = {
+        R.id.button0,
+        R.id.button1,
+        R.id.button2,
+        R.id.button3,
+        R.id.button4,
+        R.id.button5,
+        R.id.button6,
+        R.id.button7,
+        R.id.button8,
+        R.id.button9,
+        R.id.button10,
+        R.id.button11
+    };
+
+    private GenericButtonClickListener genericButtonClickListener = new GenericButtonClickListener();
+    private class GenericButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            phoneNumberEditText.setText(phoneNumberEditText.getText().toString() + ((Button)view).getText().toString());
+        }
+    }
+
+    private BackspaceButtonClickListener backspaceButtonClickListener = new BackspaceButtonClickListener();
+    private class BackspaceButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String phoneNumber = phoneNumberEditText.getText().toString();
+            if (phoneNumber.length() > 0) {
+                phoneNumber = phoneNumber.substring(0, phoneNumber.length() - 1);
+                phoneNumberEditText.setText(phoneNumber);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phoneNumberEditText = findViewById(R.id.editTextId);
+
+        backspaceImageButton = findViewById(R.id.imageButtonBack);
+        backspaceImageButton.setOnClickListener(backspaceButtonClickListener);
+
+
+        for (int i = 0; i < number_buttons.length; i++) {
+            genericButton = (Button)findViewById(number_buttons[i]);
+            genericButton.setOnClickListener(genericButtonClickListener);
+        }
     }
 }
